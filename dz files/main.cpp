9cf197c84a55e2288385376ad3ee129e0240286d
Yan <yanolfirenko9@gmail.com>
2022-05-12 12:50:54 +0200

    zadanie1

--- dz files/main.cpp
+++ dz files/main.cpp
@@ -1,7 +1,38 @@
-#include<iostream>
-#include<fstream>
+#include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
+
 int main()
 {
+    ifstream file;
+    ofstream file1;
+    int n = -1, s = 0;
+    string word, word_replace, line;
+    cout << "Enter the word you want to replace: ";
+    getline(cin, word_replace);
+    cout << "Enter word: ";
+    getline(cin, word);
+    s = word_replace.size();
 
+    file.open("zadanie1.txt");
+    file1.open("zadanie1_rezult.txt");
+    if (!file.is_open())
+        cout << "Error" << endl;
+    else
+    {
+        while (getline(file, line))
+        {
+            n = -1;
+            while (true)
+            {
+                n = line.find(word_replace, ++n);
+                if (n != string::npos)
+                    line.replace(n, s, word);
+                else break;
+            }
+            file1 << line << endl;
+        }
+        file.close();
+    }
 }
